05d795cc4758083960b3fb325afb92b90099de16
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-03-29 02:51:52 +0100

    Creat a components folder for my App

---
+++ src/components/maths.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './maths.css';
+
+const Application = () => (
+  <>
+    <section className="app-container">
+      <div className="button output">0</div>
+      <div className="button">AC</div>
+      <div className="button">+/-</div>
+      <div className="button ">%</div>
+      <div className="button sign">÷</div>
+      <div className="button">7</div>
+      <div className="button">8</div>
+      <div className="button">9</div>
+      <div className="button sign">x</div>
+      <div className="button">4</div>
+      <div className="button">5</div>
+      <div className="button">6</div>
+      <div className="button sign">-</div>
+      <div className="button">1</div>
+      <div className="button">2</div>
+      <div className="button">3</div>
+      <div className="button sign">+</div>
+      <div className="button zero">0</div>
+      <div className="button sign">=</div>
+    </section>
+  </>
+);
+
+export default Application;
