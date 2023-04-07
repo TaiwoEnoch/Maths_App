5cd571c4e970f60935b507bc40185da536e75e70
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-04-07 15:29:15 +0100

    Add pages folder.

---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+const NotFound = () => (
+  <h1 className="errorPara">
+    Let&lsquo;s check your network and try again, please?
+  </h1>
+);
+
+export default NotFound;
---
+++ src/pages/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Quote = () => (
+  <div>
+    <h2 className="quota">Random Quote</h2>
+    <p className="paraText">
+      Mathematics is not about numbers, equations, computations,
+      algorithms: it is about understanding - William Paul Thurston
+    </p>
+  </div>
+);
+
+export default Quote;
