cfcd778b691371e176e87cfc922772dc21fd4dda
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-04-07 16:53:43 +0100

    Make some changes in my codebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Calculator from './components/Calculator';
 import Header from './pages/Header';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
-// import NotFound from './pages/NotFound';
 
 class App extends React.PureComponent {
   render() {
@@ -15,7 +14,6 @@ class App extends React.PureComponent {
           <Route index element={<Home />} />
           <Route path="calculator" element={<Calculator />} />
           <Route path="quote" element={<Quote />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
         </Route>
       </Routes>
     );
--- src/pages/NotFound.js
+++
@@ -1,7 +0,0 @@
-const NotFound = () => (
-  <h1 className="errorPara">
-    Let&lsquo;s check your network and try again, please?
-  </h1>
-);
-
-export default NotFound;
