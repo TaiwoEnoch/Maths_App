fddf3d739546b6f79b9231cfe36ccebca35d9ab3
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-04-07 15:30:22 +0100

    Add neccessary files to implement the requirement

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './index.css';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Header from './pages/Header';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+// import NotFound from './pages/NotFound';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Maths App</h1>
-        </header>
-        <Calculator />
-      </div>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Route>
+      </Routes>
     );
   }
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,16 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Button = (props) => {
+  const {
+    name, className, computeFunction, colSpan,
+  } = props;
 
-  render() {
-    const {
-      name, className, computeFunction, colSpan,
-    } = this.props;
-    return (
-      <td className={className} colSpan={colSpan}>
-        <button type="button" onClick={computeFunction} name={name}>{name}</button>
-      </td>
-    );
-  }
-}
+  return (
+    <td className={className} colSpan={colSpan}>
+      <button type="button" onClick={computeFunction} name={name}>{name}</button>
+    </td>
+  );
+};
 
 Button.propTypes = {
   name: PropTypes.node.isRequired,
