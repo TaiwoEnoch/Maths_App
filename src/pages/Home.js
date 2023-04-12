5f3fd06343e31b20ce782ade40a0a02b218beb3c
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-04-12 10:34:15 +0100

    Add more styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import '../index.css';
 
 const Home = () => (
   <div className="heroPage">
+    <h1 className="heading">Welcome to Maths App!</h1>
     <p className="paraText">
       Maths App is a fun app that eases your daily need transactionary or fun needs for
       computing. This app is fully functional and accurate with figures.
