67db272388fa75cefe3ffc34c358ec0c07ffc7dc
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-04-27 12:42:06 +0100

    Build repairs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './index.css';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Header from './pages/Header';
 import Home from './pages/Home';
@@ -9,13 +9,15 @@ import Quote from './pages/Quote';
 class App extends React.PureComponent {
   render() {
     return (
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
-          <Route path="calculator" element={<Calculator />} />
-          <Route path="quote" element={<Quote />} />
-        </Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="calculator" element={<Calculator />} />
+            <Route path="quote" element={<Quote />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
